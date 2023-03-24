dc0b449ed18d874d56d0fb25a853496d28307188
alu0101130675 <alu0101130675@ull.edu.es>
2023-03-24 16:44:32 +0000

    inicio de sesion y registro casi terminado

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,18 +1,22 @@
 import { useState } from 'react'
-import { login } from '../services/login'
+import { login, signup } from '../services/login'
 import '../Login.css'
 import { Link } from 'react-router-dom'
-
 export function Login () {
   const [loginFlag, setLoginFlag] = useState(true)
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
+  const [error, setError] = useState(null)
+
   const handleLogin = async (event) => {
     event.preventDefault()
     try {
-      const response = await login({ email, password })
+      const response = loginFlag ? await login({ email, password }) : await signup({ email, password })
+
       console.log(response)
+      response.message && setError(response.message)
+      response.token && window.sessionStorage.setItem('token', response.token)
       setUser(response)
       setEmail('')
       setPassword('')
@@ -23,6 +27,7 @@ export function Login () {
   return (
     <div>
       <h1>{loginFlag ? 'Sign' : 'Log'} in to Women Info</h1>
+      {error && <h3 className='error'>{error}</h3>}
       <form className='form-container' onSubmit={(event) => handleLogin(event)}>
         <input name='email' placeholder='Email' type='email' value={email} onChange={({ target }) => setEmail(target.value)} />
         <input autoFocus type='password' placeholder='Password' value={password} onChange={({ target }) => setPassword(target.value)} />
@@ -31,7 +36,12 @@ export function Login () {
           ¿{loginFlag
           ? 'No tienes cuenta'
           : 'Ya tienes una cuenta'}?
-          <Link className='link-creta-account' onClick={() => setLoginFlag(!loginFlag)}>{loginFlag
+          <Link
+            className='link-creta-account' onClick={() => {
+              setLoginFlag(!loginFlag)
+              setError(null)
+            }}
+          >{loginFlag
             ? 'Crea una'
             : 'Inicia sesión'}
           </Link>
--- src/services/login.js
+++ src/services/login.js
@@ -1,6 +1,9 @@
 import axios from 'axios'
-const basicUrl = 'http://localhost:3001/login'
 export async function login (credentials) {
-  const { data } = await axios.post(basicUrl, credentials)
+  const { data } = await axios.post('http://localhost:3001/login', credentials)
+  return data
+}
+export async function signup (credentials) {
+  const { data } = await axios.post('http://localhost:3001/user', credentials)
   return data
 }
