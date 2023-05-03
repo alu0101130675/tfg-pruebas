f69547bfcc09b687b7fb5b5c08ec0948ff010626
alu0101130675 <alu0101130675@ull.edu.es>
2023-05-03 10:06:39 +0100

    modified comunidad Atunoma select

--- src/components/MapFilter.jsx
+++ src/components/MapFilter.jsx
@@ -1,6 +1,7 @@
 export function MapFilter ({ options, setFilter }) {
   return (
     <select
+      style={{ padding: '2px', borderRadius: '5px', backgroundColor: 'white', border: '1px solid black' }}
       size={1} onChange={({ target }) => setFilter(prevState => ({ ...prevState, comunidadAutonoma: target.value }))}
     >
       {options.map(option =>
