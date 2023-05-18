e91362cdebe24c1fbe85d7414398420a5eec58d5
alu0101130675 <alu0101130675@ull.edu.es>
2023-05-18 19:58:05 +0100

    improve chart ticks

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -10,6 +10,24 @@ import {
 
 } from 'recharts'
 import { COLORS } from '../consts'
+const CustomTick = (props) => {
+  const { x, y, payload } = props
+
+  return (
+    <g transform={`translate(${x},${y})`}>
+      <text
+        x={0}
+        y={0}
+        dy={16}
+        textAnchor='end'
+        fill='#666'
+        transform='rotate(-35)'
+      >
+        {payload.value.length < 21 ? payload.value : payload.value.slice(0, 19) + '...'}
+      </text>
+    </g>
+  )
+}
 
 export const Chart = ({ dataset, keyFields, stackFlag = undefined }) => {
   const keys = Object.keys(keyFields)
@@ -18,13 +36,14 @@ export const Chart = ({ dataset, keyFields, stackFlag = undefined }) => {
       width='100%' aspect={window.outerWidth < 600 ? 1 / 1 : 3 / 1}
     >
       <BarChart
+        margin={{ top: 5, right: 30, bottom: 100, left: 20 }}
         data={dataset}
       >
         <CartesianGrid strokeDasharray='3 3' />
-        <XAxis dataKey={dataset[0].xField} />
+        <XAxis dataKey={dataset[0].xField} tick={<CustomTick />} interval={0} />
         <YAxis />
         <Tooltip />
-        <Legend />
+        <Legend wrapperStyle={{ top: '90%', right: 0, textAlign: 'right' }} />
         {keys.map((key, i) => <Bar key={key} stackId={stackFlag} dataKey={key} fill={COLORS[i]} />)}
       </BarChart>
     </ResponsiveContainer>
--- src/components/HorizontalBarChart.jsx
+++ src/components/HorizontalBarChart.jsx
@@ -10,6 +10,23 @@ import {
 
 } from 'recharts'
 import { COLORS } from '../consts'
+const CustomTick = (props) => {
+  const { x, y, payload } = props
+
+  return (
+    <g transform={`translate(${x},${y})`}>
+      <text
+        x={0}
+        y={0}
+        dy={16}
+        textAnchor='end'
+        fill='#666'
+      >
+        {payload.value.length < 21 ? payload.value : payload.value.slice(0, 19) + '...'}
+      </text>
+    </g>
+  )
+}
 
 export function HorizontalBarChart ({ dataset, keyFields, stackFlag = undefined }) {
   const keys = Object.keys(keyFields)
@@ -21,10 +38,11 @@ export function HorizontalBarChart ({ dataset, keyFields, stackFlag = undefined 
       <BarChart
         data={dataset}
         layout='vertical'
+        margin={{ top: 2, right: -5, bottom: 0, left: 120 }}
       >
         <CartesianGrid strokeDasharray='3 3' />
         <XAxis type='number' />
-        <YAxis type='category' dataKey={dataset[0].xField} tickFormatter={(valie) => valie.substring(0, 5) + '...'} />
+        <YAxis type='category' dataKey={dataset[0].xField} tick={<CustomTick />} />
         <Tooltip />
         <Legend />
         {keys.map((key, i) => <Bar key={key} stackId={stackFlag} dataKey={key} fill={COLORS[i]} />)}
