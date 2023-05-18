f4bef50a5ec6deb35d1702d8e5769f43fbe48804
alu0101130675 <alu0101130675@ull.edu.es>
2023-05-18 17:23:11 +0100

    change weighing switch by object

--- src/logic/weighing.js
+++ src/logic/weighing.js
@@ -1,4 +1,4 @@
-/* const WEIGHHINGIN = {
+const WEIGHHINGIN = {
   'etapa A': {
     Hombre: 1595 / 31,
     Mujer: 314 / 42
@@ -11,23 +11,12 @@
     Hombre: 516 / 20,
     Mujer: 261 / 25
   }
-} */
+}
 export function wheighing (data) {
   let result = 0
   data.forEach(element => {
-    switch (element.etapa) {
-      case 'Etapa A':
-        result += element.Sexo === 'Mujer' ? 314 / 42 : 1595 / 31
-        break
-      case 'Etapa B':
-        result += element.Sexo === 'Mujer' ? 2040 / 99 : 5481 / 99
-        break
-      case 'Etapa C':
-        result += element.Sexo === 'Mujer' ? 261 / 25 : 516 / 20
-        break
-      default:
-        result += element.Sexo === 'Mujer' ? 615 / 48 : 1156 / 42
-    }
-  })
+    result += WEIGHHINGIN['etapa A'][element.Sexo]
+  }
+  )
   return result
 }
