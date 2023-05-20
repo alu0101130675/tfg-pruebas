2088866d7cd701ece0539f73aeb30d330a280761
alu0101130675 <alu0101130675@ull.edu.es>
2023-05-20 11:27:31 +0100

    adjust size of horizontal barchart

--- src/components/HorizontalBarChart.jsx
+++ src/components/HorizontalBarChart.jsx
@@ -12,7 +12,7 @@ import {
 import { COLORS } from '../consts'
 const CustomTick = (props) => {
   const { x, y, payload } = props
-  const cutSize = window.outerWidth < 600 ? 8 : 21
+  const cutSize = window.outerWidth < 600 ? 18 : 21
 
   return (
     <g transform={`translate(${x},${y})`}>
@@ -22,6 +22,7 @@ const CustomTick = (props) => {
         dy={16}
         textAnchor='end'
         fill='#666'
+        style={{ fontSize: `${window.outerWidth < 600 ? '10px' : '1rem'}` }}
       >
         {payload.value.length < cutSize ? payload.value : payload.value.slice(0, cutSize - 2) + '...'}
       </text>
@@ -39,7 +40,7 @@ export function HorizontalBarChart ({ dataset, keyFields, stackFlag = undefined 
       <BarChart
         data={dataset}
         layout='vertical'
-        margin={{ top: 2, right: -5, bottom: 0, left: window.outerWidth < 600 ? 10 : 120 }}
+        margin={{ top: 2, right: -5, bottom: 0, left: window.outerWidth < 600 ? 30 : 120 }}
       >
         <CartesianGrid strokeDasharray='3 3' />
         <XAxis type='number' />
